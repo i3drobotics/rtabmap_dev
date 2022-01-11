43ea605ef051c3e5c960e3d791640b05814d4beb
Ben <benknight135@gmail.com>
2022-01-11 17:36:07 +0000

    init commit

---
+++ build_i3dr.sh
@@ -0,0 +1,3 @@
+docker build -t i3drobotics/rtabmap:bionic_deps ./rtabmap_bionic_deps
+docker build -t i3drobotics/rtabmap:bionic_dev ./rtabmap_bionic_dev
+docker build -t i3drobotics/rtabmap:bionic ./rtabmap_bionic
